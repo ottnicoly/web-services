4a10580a255287493de0a43dad7ba49718a75b12
Nicoly Ott <nicolyott23@gmail.com>
2024-11-05 15:05:50 -0300

    Service layer, component registration

--- src/main/java/com/nicoly/webServices/repositories/UserRepository.java
+++ src/main/java/com/nicoly/webServices/repositories/UserRepository.java
@@ -2,7 +2,10 @@ package com.nicoly.webServices.repositories;
 
 import com.nicoly.webServices.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+//uso opcional pois herda a anotação de jparepository
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
