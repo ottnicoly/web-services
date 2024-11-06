f5338006dd6cd161486984fc4e279c469bf6c256
Nicoly Ott <nicolyott23@gmail.com>
2024-11-06 14:43:26 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/nicoly/webServices/entities/OrderItem.java
+++ src/main/java/com/nicoly/webServices/entities/OrderItem.java
@@ -29,7 +29,7 @@ public class OrderItem implements Serializable {
         this.quantity = quantity;
         this.price = price;
     }
- 
+
     @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
